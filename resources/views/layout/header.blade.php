45918da2782a8d2117444303ec7df97c9428f9f1
faniabdo99 <faniabdo99@gmail.com>
2022-06-03 15:03:35 +0200

    Revert Update

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="shortcut icon" href="{{url('public/img/favicon.ico')}}" />
-    <title>Iraqiiiiii Heritage Boat Club</title>
+    <title>Iraqi Heritage Boat Club</title>
     <!-- styles-->
     <link rel="stylesheet" href="{{url('public/css/styles.min.css')}}" />
     @if(app()->getLocale() == 'ar')
