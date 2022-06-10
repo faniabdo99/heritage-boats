2f02c2c496593d0fe0653e12aca456205a7d5b62
TDESIGN96 <tayseer996@gmail.com>
2022-06-10 22:01:44 +0300

    update clubs and calendar

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -12,4 +12,7 @@
         <link rel="stylesheet" href="{{url('public/css/styles-ara.css')}}" />
     @endif
     <link rel="stylesheet" href="{{url('public/css/animate.css')}}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
