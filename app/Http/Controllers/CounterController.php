e67c06bd9eb66a1536d09495b110126e8c98c9d1
faniabdo99 <faniabdo99@gmail.com>
2022-06-23 01:20:30 +0200

    Heros

--- app/Http/Controllers/CounterController.php
+++ app/Http/Controllers/CounterController.php
@@ -29,63 +29,28 @@ class CounterController extends Controller{
     public function postHero(Request $r){
         $HeroData = [];
         if($r->has('homepage')){
-            //Handle image upload
-            //Resize the image
             $img = ImageLib::make($r->homepage);
-            // backup status
-            $img->backup();
-            $img->fit(600, 600);
-            //Upload to server
             $img->save('storage/app/public/images/heros/homepage.'.$r->homepage->getClientOriginalExtension());
-            //Return the filename
             $HeroData['homepage'] = 'homepage.'.$r->homepage->getClientOriginalExtension();
         }
         if($r->has('about')){
-            //Handle image upload
-            //Resize the image
             $img = ImageLib::make($r->about);
-            // backup status
-            $img->backup();
-            $img->fit(600, 600);
-            //Upload to server
             $img->save('storage/app/public/images/heros/about.'.$r->about->getClientOriginalExtension());
-            //Return the filename
             $HeroData['about'] = 'about.'.$r->about->getClientOriginalExtension();
         }
         if($r->has('contact')){
-            //Handle image upload
-            //Resize the image
             $img = ImageLib::make($r->contact);
-            // backup status
-            $img->backup();
-            $img->fit(600, 600);
-            //Upload to server
             $img->save('storage/app/public/images/heros/contact.'.$r->contact->getClientOriginalExtension());
-            //Return the filename
             $HeroData['contact'] = 'contact.'.$r->contact->getClientOriginalExtension();
         }
         if($r->has('acknowledgments')){
-            //Handle image upload
-            //Resize the image
             $img = ImageLib::make($r->acknowledgments);
-            // backup status
-            $img->backup();
-            $img->fit(600, 600);
-            //Upload to server
             $img->save('storage/app/public/images/heros/acknowledgments.'.$r->acknowledgments->getClientOriginalExtension());
-            //Return the filename
             $HeroData['acknowledgments'] = 'acknowledgments.'.$r->acknowledgments->getClientOriginalExtension();
         }
         if($r->has('calendar')){
-            //Handle image upload
-            //Resize the image
             $img = ImageLib::make($r->calendar);
-            // backup status
-            $img->backup();
-            $img->fit(600, 600);
-            //Upload to server
             $img->save('storage/app/public/images/heros/calendar.'.$r->calendar->getClientOriginalExtension());
-            //Return the filename
             $HeroData['calendar'] = 'calendar.'.$r->calendar->getClientOriginalExtension();
         }
         Counter::updateOrCreate(
